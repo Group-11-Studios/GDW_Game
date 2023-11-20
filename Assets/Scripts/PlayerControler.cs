4396db9f10b9a14a8a4dac3af03d93e8dd9425c6
Shallowcat <joshR364@hotmail.com>
2023-11-20 15:31:13 -0500

    mostly working oneways

--- Assets/Scripts/PlayerControler.cs
+++ Assets/Scripts/PlayerControler.cs
@@ -22,6 +22,8 @@ public class PlayerControler : MonoBehaviour
 
     bool facingRight = true;
     float lastDash = 0.6f;
+
+    bool crouched = false;
     
 
     void FixedUpdate()
@@ -42,7 +44,7 @@ public class PlayerControler : MonoBehaviour
         }
         else if (input.y < 0)
         {
-            // Call Crouch Func Here
+            crouched = true;
         }
 
         // Update facing direction
@@ -61,6 +63,11 @@ public class PlayerControler : MonoBehaviour
 
     public void OnJump()
     {
+        if (crouched) 
+        { 
+            // Check For OneWay Colision
+        }
+
         // Check if player can jump (is on the ground or has jumped once and has not landed)
         bool check = IsGrounded();
         if (check && doubleJump)
